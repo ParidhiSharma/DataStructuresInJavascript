4badf048a273a0f1219455c666bbf2feeed35e79
ParidhiSharma <pari30989@gmail.com>
2017-03-28 21:55:35 +0530

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -1,86 +1,128 @@
-/**
- * Created by paridhisharma on 11/11/16.
- */
+import Node from './Node'
 
-/*Class to create a Node and Linked list in ES6*/
+export default class LinkedList{
+	constructor() {
+		this.head = null;
+	}
 
-class Node {
-    constructor(value) {
-        this.val = value;
-        this.next = null;
-    }
+	insert(nodeValue) {
+		let node = this.head;
+		if(node) {
+			while(node.next) {
+				node = node.next;
+			}
+			node.next = new Node(nodeValue);
+		} else {
+			this.head = new Node(nodeValue);
+		}
+	}
+
+	insertFromHead(nodeValue) {
+		let head = this.head;
+		if(head) {
+			let node = new Node(nodeValue);
+			node.next = head;
+			this.head = node;
+		} else {
+			this.head = new Node(nodeValue);
+		}
+	}
+
+	deleteHead() {
+		let head = this.head;
+		if(head && head.next) {
+			let node = head.next;
+			head.next = null;
+			this.head = node;
+		}
+	}
+
+	deleteByValue(nodeValue) {
+		if(!this.head)
+			return 'NO head';
+
+		let node = this.head, prev = null;
+		while(node.next!=null) {
+			if(node.value !== nodeValue) {
+				prev = node;
+				node = node.next;
+			} else {
+				break;
+			}
+		}
+		if(node.next == null) {
+			return 'No element found'
+		}
+		let temp = node.next;
+		prev.next = temp;
+		return 'Deleted element';
+	}
+
+	reverseLLInSectionsOfK(node) {
+		if(!node)
+			return null;
+
+		let k =0, prev = null, first= node;
+
+		while(node != null && k <3) {
+			let temp = node.next;
+			node.next = prev;
+			prev = node;
+			node = temp;
+			k++;
+		}
+		first.next = this.reverseLLInSectionsOfK(node);
+		return prev;
+	}
+
+	searchByValue(value) {
+		if(!this.head)
+			return 'NO head';
+
+		let node = this.head, i=0;
+		while(node) {
+			if(node.value == value) {
+				return i;
+			}
+			i++;
+			node = node.next;
+		}
+		return 'Element not found';
+	}
+
+
+	  findIntersection(l1, l2) {
+		if(!l1.head || !l2.head) {
+			return;
+		}
+
+		let node1 =  l1.head, node2 = l2.head, len1=0, len2=0, prev1 = null, prev2 = null;
+		while(node1 != null) {
+			prev1 = node1;
+			node1 = node1.next;
+			len1++;
+		}
+
+		while(node2 != null) {
+			prev2 = node2;
+			node2 = node2.next;
+			len2++;
+		}
+
+		if(prev1.value == prev2.value) {
+			node1 = len1>len2? l1.head : l2.head;
+			let diff = len1 > len2? len1 - len2 : len2 - len1;
+
+			while(diff >= 0) {
+        		diff--;
+				node1 = node1.next;
+			}
+			console.log(node1);
+			return node1.value;
+		}
+		return 'No Intersection'
+	}
+
+
+
 }
-
-class LinkedList  {
-    constructor() {
-        this.root = null;
-    }
-
-    push(value) {
-        if(value === undefined) {
-            return;
-        }
-
-        let root = this.root;
-        const node = new Node(value);
-        if(!root) {
-            this.root = new Node(value);
-            return;
-        }
-        while(root.next) {
-            root = root.next;
-        }
-        root.next = node;
-    };
-
-    pop() {
-        let root = this.root;
-        if(!root) {
-            return;
-        }
-        this.root = root.next;
-    };
-
-    reverse() {
-        if(!this.root) {
-            return;
-        }
-        let prev, curr = this.root, next;
-        while(curr) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        this.root = prev;
-    };
-
-    printList() {
-        var root = this.root;
-        if(!root) {
-            return;
-        }
-        while(root) {
-            console.log(root.val);
-            root = root.next;
-        }
-    };
-}
-
-const linkedList1 = new LinkedList();
-linkedList1.push(1);
-linkedList1.push(2);
-linkedList1.push(3);
-linkedList1.push(4);
-linkedList1.push(5);
-linkedList1.push(6);
-linkedList1.push(7);
-linkedList1.push(8);
-linkedList1.push(9);
-linkedList1.push(10);
-
-linkedList1.printList();
-linkedList1.reverse();
-linkedList1.printList();
-
-
